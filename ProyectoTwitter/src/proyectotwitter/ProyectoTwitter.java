51bea47bcd7692a711581a3e46e369ed0285375b
Julio <juliopatriciodasilva@gmail.com>
2016-04-18 09:00:25 +0200

    inicial sin readme

---
+++ ProyectoTwitter/src/proyectotwitter/ProyectoTwitter.java
@@ -0,0 +1,18 @@
+
+package proyectotwitter;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+
+/**
+ *
+ * @author JP
+ */
+public class ProyectoTwitter {
+
+   
+    public static void main(String[] args) {
+        Twitter mitwitter = new TwitterFactory().getInstance();
+    }
+    
+}
